8a531c91c4281708a6537f34beb9befe581eb383
Richard Mikloš <akisytbsteamy@gmail.com>
2025-01-24 20:56:14 +0100

    Initial commit

---
+++ src/main/java/org/notionsmp/nexoExtras/NexoExtras.java
@@ -0,0 +1,14 @@
+package org.notionsmp.nexoExtras;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NexoExtras extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
